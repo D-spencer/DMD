8df41611428497a3bfdc99bc9e21a7932cc7170a
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2026-04-13 20:43:40 +0100

    Update app.py

--- dmd_center/app.py
+++ dmd_center/app.py
@@ -173,17 +173,18 @@ with tab2:
     # -------------------------------
         if st.button("Predict (Advanced)"):
             input_data = pd.DataFrame([{
-            "age": age,
-            "sex": 1 if sex == "Male" else 0,
-            "chestpaintype": cp_map[chest_pain],
-            "restingbp": resting_bp,
-            "cholesterol": cholesterol,
-            "fastingbs": fbs_map[fasting_bs],
-            "restingecg": ecg_map[resting_ecg],
-            "max_hr": max_hr,
-            "exercise_angina": angina_map[exercise_angina],
-            "old_peak": old_peak,
-            "st_slope": slope_map[st_slope] }])
+    "age": int(age),
+    "sex": int(1 if sex == "Male" else 0),
+    "chestpaintype": str(cp_map[chest_pain]),
+    "restingbp": float(resting_bp),
+    "cholesterol": float(cholesterol),
+    "fastingbs": int(fbs_map[fasting_bs]),
+    "restingecg": str(ecg_map[resting_ecg]),
+    "max_hr": float(max_hr),
+    "exercise_angina": str(angina_map[exercise_angina]),
+    "old_peak": float(old_peak),
+    "st_slope": str(slope_map[st_slope])
+}])
             
             pred = heart_adv_model.predict(input_data)[0]
             prob = heart_adv_model.predict_proba(input_data)[0][1]
