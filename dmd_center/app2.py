fec0951491e5c1c6caf835f9eae46e1bbc4f86bf
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2026-04-13 19:25:22 +0100

    Update app2.py

--- dmd_center/app2.py
+++ dmd_center/app2.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pickle
 
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 st.write("BASE DIR:", BASE_DIR)
 st.write("FILES:", os.listdir(BASE_DIR))
 st.write("MODELS:", os.listdir(os.path.join(BASE_DIR, "models")))
