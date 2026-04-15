bb909721aee6e6a3d9d58a8b18e417ac76bcf290
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2026-04-15 08:57:04 +0100

    Update app.py

--- dmd_center/app.py
+++ dmd_center/app.py
@@ -139,9 +139,8 @@ with tab2:
              "chestpaintype": cp_map[chest_pain],
              "restingbp": resting_bp,
              "cholesterol": cholesterol,
-             "exercise_angina": angina_map[exercise_angina]
-            
-         }])
+             "exercise_angina": angina_map[exercise_angina]  }])
+              
                pred = heart_basic_model.predict(input_data)[0]
                prob = heart_basic_model.predict_proba(input_data)[0][1]
                
